74967e829b9ab0eb883e852415de29722e6dd74f
zhouheling <641668410@qq.com>
2015-12-11 16:18:55 +0800

    Homepage show which report can use now

--- GeneralReport/utils.py
+++ GeneralReport/utils.py
@@ -7,3 +7,17 @@ def replace_invalid_quote(input_str):
     input_str = input_str.replace(u"”",'"')
     input_str = input_str.replace(u"“",'"')
     return input_str
+
+class ObjectDict(dict):
+    """
+    Makes a dictionary behave like an object, with attribute-style access.
+    """
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
--- GeneralReport/views.py
+++ GeneralReport/views.py
@@ -10,9 +10,48 @@ from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import PasswordChangeForm
 
+from GeneralReport.utils import ObjectDict
+
 @login_required
 def index(request):
-    return render(request, 'GeneralReport/index.html')
+    user = request.user
+    report_solo_perm_list = user.get_all_report_permissions()
+    report_comb_perm_list = user.get_all_report_combination_permissions()
+
+    # single report
+    report_solo_info_dict = ObjectDict()
+    for solo_obj in report_solo_perm_list:
+        solo_class = solo_obj.report_type.name if solo_obj.report_type else u'Unclassified'
+        solo_url = request.build_absolute_uri('/sgrs/report/%s' %solo_obj.name)
+        solo_desc = solo_obj.description
+        solo_info = ObjectDict(
+            name = solo_obj.name,
+            desc = solo_desc,
+            report_url = solo_url,
+        )
+        report_solo_info_dict.setdefault(solo_class,[]).append(solo_info)
+
+    for key in report_solo_info_dict:
+        report_solo_info_dict[key].sort(key=lambda x:x.desc)
+
+    # combination report
+    report_comb_info_list = []
+    for comb_obj in report_comb_perm_list:
+        comb_url = request.build_absolute_uri('/sgrs/comb/%s' %comb_obj.id)
+        comb_info = ObjectDict(
+            name = comb_obj.name,
+            desc = comb_obj.description,
+            report_url = comb_url,
+        )
+        report_comb_info_list.append(comb_info)
+    report_comb_info_list.sort(key=lambda x:x.desc)
+
+    data = {
+        'report_solo_info_dict':report_solo_info_dict,
+        'report_comb_info_list':report_comb_info_list,
+    }
+
+    return render(request, 'GeneralReport/index.html', data)
 
 def login(request):
     form = AuthenticationForm()
